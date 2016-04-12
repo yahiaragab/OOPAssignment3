4f85f04f56ca5bc2cde1b800358d3d8896b87997
podgemccarthy <padraigmccarthy1996@gmail.com>
2016-04-12 17:03:37 +0100

    Gps enabled check

--- mapstest/app/src/main/java/com/example/pmccarthy/mapstest/GPSTracker.java
+++ mapstest/app/src/main/java/com/example/pmccarthy/mapstest/GPSTracker.java
@@ -43,8 +43,7 @@ public class GPSTracker extends Service implements LocationListener
 
     public Location getLocation()
     {
-        try
-        {
+        try {
             //initializing the location manager
             locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
             //initializing boolean variable
@@ -52,29 +51,23 @@ public class GPSTracker extends Service implements LocationListener
             //initializing boolean variable
             isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-            if(!isGPSEnabled && !isNetworkEnabled)
-            {
+            if (!isGPSEnabled && !isNetworkEnabled) {
 
-            }
-            else
-            {
+            } else {
                 this.canGetLocation = true;
 
-                if (isNetworkEnabled)
-                {
+                if (isNetworkEnabled) {
 
                     locationManager.requestLocationUpdates(
                             LocationManager.NETWORK_PROVIDER,
                             MIN_TIME_BW_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 
-                    if (locationManager != null)
-                    {
+                    if (locationManager != null) {
                         location = locationManager
                                 .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
-                        if (location != null)
-                        {
+                        if (location != null) {
 
                             latitude = location.getLatitude();
                             longitude = location.getLongitude();
@@ -82,9 +75,27 @@ public class GPSTracker extends Service implements LocationListener
                     }
 
                 }
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
 
             }
-        catch (Exception e)
+        }
+        catch(Exception e)
         {
             //error handling
             e.printStackTrace();
